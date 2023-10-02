72d98afa0192aa000ae07934d2f12bc73dcc2d39
Woong99 <dndry1072@naver.com>
2023-10-02 21:30:55 +0900

    2023.10.02 - 프로젝트 생성

---
+++ form-login-without-security/src/main/java/com/example/formloginwithoutsecurity/FormLoginWithoutSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.formloginwithoutsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormLoginWithoutSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormLoginWithoutSecurityApplication.class, args);
+    }
+
+}
---
+++ form-login-without-security/src/test/java/com/example/formloginwithoutsecurity/FormLoginWithoutSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formloginwithoutsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormLoginWithoutSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
